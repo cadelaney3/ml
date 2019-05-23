a09c5e60041a8e68bd4393d6e122eb829157775b
Chris Delaney <cadelaney3@gmail.com>
2019-05-23 15:28:19 -0600

    added matrix multiply and add

--- go/linear_regression/main.go
+++ go/linear_regression/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"bufio"
+	"errors"
 	"fmt"
 	"os"
 	"strconv"
@@ -18,7 +19,47 @@ func hypothesis(x float32, y float32) {
 
 }
 
-//func cost()
+func matAddFloat32(mat1 [][]float32, mat2 [][]float32) ([][]float32, error) {
+	if len(mat1) != len(mat2) && len(mat1[0]) != len(mat2[0]) {
+		return nil, errors.New("invalid matrix dimensions for matrix addition")
+	}
+
+	result := make([][]float32, len(mat1))
+	for i := 0; i < len(mat1); i++ {
+		result[i] = make([]float32, len(mat1[0]))
+		for j := 0; j < len(mat1); j++ {
+			result[i][j] = mat1[i][j] + mat2[i][j]
+		}
+	}
+	return result, nil
+}
+
+func matMultFloat32(mat1 [][]float32, mat2 [][]float32) ([][]float32, error) {
+	if len(mat1[0]) != len(mat2) {
+		return nil, errors.New("invalid matrix dimensions for matrix multiplication")
+	}
+	//fmt.Printf("len mat1: %d; len mat2: %d\n", len(mat1), len(mat2))
+	result := make([][]float32, len(mat1))
+	for i := 0; i < len(mat1); i++ {
+		result[i] = make([]float32, len(mat2[0]))
+		for j := 0; j < len(mat1); j++ {
+			for k := 0; k < len(mat1); k++ {
+				result[i][j] += mat1[i][k] * mat2[k][j]
+			}
+		}
+	}
+	return result, nil
+}
+
+func transpose(mat [][]float32) [][]float32 {
+	result := make([][]float32, len(mat[0]))
+	for i := 0; i < len(mat); i++ {
+		for j := 0; j < len(mat[0]); j++ {
+			result[j] = append(result[j], mat[i][j])
+		}
+	}
+	return result
+}
 
 func main() {
 
@@ -82,27 +123,33 @@ func main() {
 
 	epochs := 0
 	for epochs < 1000 {
-		var y float32
-		var e float32
-		var meanSqErr float32
-		var meanSqTemp float32
-		y = a0 + a1*xTrain
-		e = y - yTrain
-		for _, val := range e {
-			meanSqTemp += val
-		}
-		errX := e * xTrain
-		var sumErrX float32
-		for _, val := range errX {
-			sumErrX += val
-		}
-		meanSqErr = meanSqTemp * meanSqTemp
-		meanSqErr = meanSqErr / n
-		a0 = a0 - alpha*2*meanSqTemp/n
-		a1 = a1 - alpha*2*sumErrX/n
-		epochs += 1
-		if epochs%10 == 0 {
-			fmt.Println(meanSqErr)
-		}
+		// var y float32
+		// var e float32
+		// var meanSqErr float32
+		// var meanSqTemp float32
+		//y = a0 + a1*xTrain
+		a2 := transpose(a1)
+		x, err := matMultFloat32(a2, xTrain)
+		check(err)
+		fmt.Println(x)
+		/*
+			e = y - yTrain
+			for _, val := range e {
+				meanSqTemp += val
+			}
+			errX := e * xTrain
+			var sumErrX float32
+			for _, val := range errX {
+				sumErrX += val
+			}
+			meanSqErr = meanSqTemp * meanSqTemp
+			meanSqErr = meanSqErr / n
+			a0 = a0 - alpha*2*meanSqTemp/n
+			a1 = a1 - alpha*2*sumErrX/n
+			epochs += 1
+			if epochs%10 == 0 {
+				fmt.Println(meanSqErr)
+			}
+		*/
 	}
 }
